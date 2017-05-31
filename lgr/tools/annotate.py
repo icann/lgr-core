cf63dfdb2ffd80b11b86b3269ab4d337d7af78f9
Julien BERNARD <julien.bernard@viagenie.ca>
2017-05-31 15:58:11 -0400

    Adapt annotate and diff comparison tools to handle LGR sets

--- lgr/tools/annotate.py
+++ lgr/tools/annotate.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 import logging
 
 from lgr.tools.utils import read_labels
+from lgr.tools.diff_collisions import get_collisions
 
 logger = logging.getLogger(__name__)
 
@@ -22,3 +23,33 @@ def annotate(lgr, labels_input):
         label_cp = tuple([ord(c) for c in label])
         disp = lgr.test_label_eligible(label_cp, collect_log=False)[3]
         yield "%s: %s\n" % (label, disp)
+
+
+def lgr_set_annotate(lgr, script_lgr, set_labels, labels_input):
+    """
+    Annotate a list of labels with their disposition.
+
+    :param lgr: The LGR set object.
+    :param script_lgr: The LGR object for the script used to check label validity.
+    :param set_labels: The labels in the lgr set
+    :param labels_input: The file containing the labels
+    """
+    for label in read_labels(labels_input, script_lgr.unicode_database):
+        label_cp = tuple([ord(c) for c in label])
+        (eligible, _, _, disp, _, _) = script_lgr.test_label_eligible(label_cp, collect_log=False)
+        collision = ''
+        if eligible:
+            if label in set_labels:
+                collision = 'Label is in the LGR set labels'
+            # check for collisions
+            indexes = get_collisions(lgr, set_labels + [label], quiet=False)
+            if len(indexes) > 0:
+                collision = 'Label collides with the LGR set labels'
+
+        out = disp
+        if collision:
+            # TODO do we need to change disp to invalid???
+            out = '{} - {}'.format(disp, collision)
+
+        yield "%s: %s\n" % (label, out)
+
--- lgr/tools/compare/__init__.py
+++ lgr/tools/compare/__init__.py
@@ -3,6 +3,6 @@
 __init__.py - Definition of various comparison functions LGRs.
 """
 
-from lgr.tools.compare.diff import diff_lgrs
+from lgr.tools.compare.diff import diff_lgrs, diff_lgr_sets
 from lgr.tools.compare.intersect import intersect_lgrs
 from lgr.tools.compare.union import union_lgrs
