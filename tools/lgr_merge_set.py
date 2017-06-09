bc3a9196f0c50af57bcfdc3079cc56a2d1945c4a
Audric Schiltknecht <audric.schiltknecht@viagenie.ca>
2017-06-09 11:42:26 -0400

    Update CLI tool to merge LGR
    - Make ICU lib optional.
    - Fix writing XML output to console: XML is already encoded.

--- tools/lgr_merge_set.py
+++ tools/lgr_merge_set.py
@@ -15,7 +15,7 @@ from munidata import UnicodeDataVersionManager
 
 from lgr.parser.xml_serializer import serialize_lgr_xml
 
-from lgr.tools.utils import write_output, merge_lgrs
+from lgr.tools.utils import merge_lgrs
 
 logger = logging.getLogger("lgr_merge_set")
 
@@ -23,7 +23,7 @@ logger = logging.getLogger("lgr_merge_set")
 def main():
     parser = argparse.ArgumentParser(description='LGR diff and collision CLI')
     parser.add_argument('-v', '--verbose', action='store_true', help='be verbose')
-    parser.add_argument('-l', '--libs', metavar='LIBS', help='ICU libraries', required=True)
+    parser.add_argument('-l', '--libs', metavar='LIBS', help='ICU libraries')
     parser.add_argument('-r', '--rng', metavar='RNG', help='RelaxNG XML schema')
     parser.add_argument('-n', '--name', metavar='NAME', help="Merged LGR name")
     parser.add_argument('-s', '--lgr-set', metavar='LGR-SET', action='append',
@@ -34,9 +34,11 @@ def main():
     log_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(stream=sys.stdout, level=log_level)
 
-    libpath, i18n_libpath, libver = args.libs.split('#')
-    manager = UnicodeDataVersionManager()
-    unidb = manager.register(None, libpath, i18n_libpath, libver)
+    unidb = None
+    if args.libs:
+        libpath, i18n_libpath, libver = args.libs.split('#')
+        manager = UnicodeDataVersionManager()
+        unidb = manager.register(None, libpath, i18n_libpath, libver)
 
     if len(args.lgr_set) == 1:
         logger.error("Please provide more than one LGR to make a set")
@@ -47,7 +49,7 @@ def main():
     merged_lgr, _ = merge_lgrs(args.lgr_set, name=args.name, rng=args.rng, unidb=unidb)
     if not merged_lgr:
         return
-    write_output(serialize_lgr_xml(merged_lgr, pretty_print=True))
+    print(serialize_lgr_xml(merged_lgr, pretty_print=True))
 
 if __name__ == '__main__':
     main()
