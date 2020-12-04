07eae31b2f0c98a6bc7d3e19dbad0cfcea3f0f9e
Julien BERNARD <julien.bernard@viagenie.ca>
2020-12-04 11:37:08 -0500

    Add IDN review header section report

--- lgr/tools/idn_review/header.py
+++ lgr/tools/idn_review/header.py
@@ -5,11 +5,23 @@
 header - 
 """
 import logging
+from datetime import date
 
 from lgr.core import LGR
 
 logger = logging.getLogger('header')
 
 
-def generate_header(idn_table: LGR, reference_lgr: LGR):
-    return
+def generate_header(idn_table: LGR, reference_lgr: LGR) -> dict:
+    return {
+        'date': date.today(),
+        'disclaimer': 'TODO',
+        'idn_table': {
+            'filename': idn_table.name,
+            'version': idn_table.metadata.version.value
+        },
+        'reference_lgr': {
+            'name': reference_lgr.name,
+            'version': reference_lgr.metadata.version.value
+        }
+    }
