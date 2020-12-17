08b28d2c4acc6aa03c9f56ce24944d0eb3fd031f
Julien BERNARD <julien.bernard@viagenie.ca>
2020-12-17 10:39:59 -0500

    Add IDN review repertoire section report

--- lgr/tools/idn_review/repertoire.py
+++ lgr/tools/idn_review/repertoire.py
@@ -5,11 +5,109 @@
 repertoire - 
 """
 import logging
+from enum import auto
+from typing import Set, Tuple, Dict, List
 
+from lgr.char import Char
 from lgr.core import LGR
+from lgr.tools.idn_review.utils import AutoName
+from munidata.database import UnicodeDatabase
 
 logger = logging.getLogger(__name__)
 
 
-def generate_repertoire_report(idn_table: LGR, reference_lgr: LGR):
-    return
+class RepertoireResult(AutoName):
+    MATCH = auto()
+    SUBSET = auto()
+    NOTE = auto()
+    REVIEW = auto()
+    MANUAL_CHECK = auto()
+
+
+class RepertoireReport:
+
+    def __init__(self, idn_table_char: Char, reference_lgr_char: Char, reference_lgr_tags: Set[str],
+                 reference_lgr_rules_names: Set[str], unidb: UnicodeDatabase):
+        self.idn_table_char = idn_table_char
+        self.reference_lgr_char = reference_lgr_char
+        self.reference_lgr_tags = reference_lgr_tags
+        self.reference_lgr_rules_names = reference_lgr_rules_names
+        self.unidb = unidb
+
+    def compare_char(self) -> Tuple[RepertoireResult, str]:
+        if not self.idn_table_char:
+            return RepertoireResult.SUBSET, "Match as a subset of repertoire"
+        if not self.reference_lgr_char:
+            return (RepertoireResult.MANUAL_CHECK,
+                    "The code point only exists in the IDN Table but not in the reference LGR")
+
+        # here we merge NOTE and REVIEW on rules and tags but REVIEW is prioritized over NOTE for result
+        result = None
+        remark = ''
+        # check tags
+        for tag in set(self.idn_table_char.tags) ^ set(self.reference_lgr_char.tags):
+            if tag not in self.idn_table_char.tags:
+                result = RepertoireResult.REVIEW
+                remark = "Tags do not match"
+                break
+            if tag not in self.reference_lgr_char.tags:
+                if tag in self.reference_lgr_tags:
+                    result = RepertoireResult.REVIEW
+                    remark = "Tags do not match"
+                    break  # stop here as we have a REVIEW that is prioritized over NOTE
+                elif not result:
+                    result = RepertoireResult.NOTE
+                    remark = "Tags not required in Reference LGR"
+                    # continue in case we have a REVIEW later
+        # check rule
+        idn_rule_name = self.idn_table_char.when or self.idn_table_char.not_when
+        ref_rule_name = self.reference_lgr_char.when or self.reference_lgr_char.not_when
+        if idn_rule_name != ref_rule_name:
+            if remark:
+                remark += '\n'
+            if result != RepertoireResult.REVIEW:
+                if idn_rule_name and idn_rule_name not in self.reference_lgr_rules_names:
+                    result = RepertoireResult.NOTE
+                    remark += "Rules not required in Reference LGR"
+                    return result, remark
+                else:
+                    # reset remarks as we will get a REVIEW result
+                    remark = ''
+            result = RepertoireResult.REVIEW
+            remark += "Rules do not match"
+
+        if not result:
+            # tags and rules match
+            return RepertoireResult.MATCH, "Matches code point (including tags, context rule)"
+
+        return result, remark
+
+    def to_dict(self) -> Dict:
+        result, remark = self.compare_char()
+        char = self.idn_table_char or self.reference_lgr_char
+        return {
+            'cp': char.cp,
+            'glyph': str(char),
+            'name': " ".join(self.unidb.get_char_name(cp) for cp in char.cp),
+            'idn_table': self.idn_table_char is not None,
+            'reference_lgr': self.reference_lgr_char is not None,
+            'result': result.value,
+            'remark': remark
+        }
+
+
+def generate_repertoire_report(idn_table: LGR, reference_lgr: LGR) -> List[Dict]:
+    idn_table_repertoire: Dict[Tuple, Char] = {c.cp: c for c in idn_table.repertoire.all_repertoire(expand_ranges=True)}
+    reference_lgr_repertoire = {c.cp: c for c in reference_lgr.repertoire.all_repertoire(expand_ranges=True)}
+
+    reports = []
+    for cp in sorted(idn_table_repertoire.keys() | reference_lgr_repertoire.keys()):
+        idn_char = idn_table_repertoire.get(cp)
+        ref_char = reference_lgr_repertoire.get(cp)
+        unidb = idn_table.unicode_database if idn_char else reference_lgr.unicode_database
+        report = RepertoireReport(idn_char, ref_char,
+                                  set(t for t in reference_lgr.all_tags() if not t.startswith('sc:')),
+                                  set(reference_lgr.rules_lookup.keys()), unidb)
+        reports.append(report.to_dict())
+
+    return reports
---
+++ tests/unit/idn_table_review/test_repertoire.py
@@ -0,0 +1,327 @@
+#! /bin/env python
+# -*- coding: utf-8 -*-
+# Author: Viagénie
+"""
+test_variant_sets - 
+"""
+import logging
+from unittest import TestCase
+
+from lgr.tools.idn_review.repertoire import generate_repertoire_report
+from tests.unit.unicode_database_mock import UnicodeDatabaseMock
+from tests.unit.utils import load_lgr
+
+logger = logging.getLogger('test_variant_sets')
+
+
+class Test(TestCase):
+    matching_cp = [{
+        'cp': (97,),
+        'glyph': 'a',
+        'name': 'LATIN SMALL LETTER A',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (98,),
+        'glyph': 'b',
+        'name': 'LATIN SMALL LETTER B',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (99,),
+        'glyph': 'c',
+        'name': 'LATIN SMALL LETTER C',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (100,),
+        'glyph': 'd',
+        'name': 'LATIN SMALL LETTER D',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (101,),
+        'glyph': 'e',
+        'name': 'LATIN SMALL LETTER E',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (102,),
+        'glyph': 'f',
+        'name': 'LATIN SMALL LETTER F',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (103,),
+        'glyph': 'g',
+        'name': 'LATIN SMALL LETTER G',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (104,),
+        'glyph': 'h',
+        'name': 'LATIN SMALL LETTER H',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (105,),
+        'glyph': 'i',
+        'name': 'LATIN SMALL LETTER I',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (48,),
+        'glyph': '0',
+        'name': 'DIGIT ZERO',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (49,),
+        'glyph': '1',
+        'name': 'DIGIT ONE',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (50,),
+        'glyph': '2',
+        'name': 'DIGIT TWO',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (51,),
+        'glyph': '3',
+        'name': 'DIGIT THREE',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (52,),
+        'glyph': '4',
+        'name': 'DIGIT FOUR',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (53,),
+        'glyph': '5',
+        'name': 'DIGIT FIVE',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (54,),
+        'glyph': '6',
+        'name': 'DIGIT SIX',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (55,),
+        'glyph': '7',
+        'name': 'DIGIT SEVEN',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (56,),
+        'glyph': '8',
+        'name': 'DIGIT EIGHT',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (57,),
+        'glyph': '9',
+        'name': 'DIGIT NINE',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (339,),
+        'glyph': 'œ',
+        'name': 'LATIN SMALL LIGATURE OE',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }, {
+        'cp': (111, 101),
+        'glyph': 'oe',
+        'name': 'LATIN SMALL LETTER O LATIN SMALL LETTER E',
+        'idn_table': True,
+        'reference_lgr': True,
+        'result': 'MATCH',
+        'remark': 'Matches code point (including tags, context rule)'
+    }]
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.maxDiff = None
+        self.unidb = UnicodeDatabaseMock()
+        self.ref = load_lgr('idn_table_review', 'reference_lgr.xml', unidb=self.unidb)
+
+    def test_repertoire_missing_in_lgr(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_missing_in_lgr.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp + [{
+            'cp': (106,),
+            'glyph': 'j',
+            'name': 'LATIN SMALL LETTER J',
+            'idn_table': True,
+            'reference_lgr': False,
+            'result': 'MANUAL CHECK',
+            'remark': 'The code point only exists in the IDN Table but not in the reference LGR'
+        }], key=lambda x: x['cp']))
+
+    def test_repertoire_note_rule(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_note_rule.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp[:3] + self.matching_cp[4:] + [{
+            'cp': (100,),
+            'glyph': 'd',
+            'name': 'LATIN SMALL LETTER D',
+            'idn_table': True,
+            'reference_lgr': True,
+            'result': 'NOTE',
+            'remark': 'Rules not required in Reference LGR'
+        }], key=lambda x: x['cp']))
+
+    def test_repertoire_note_tag(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_note_tag.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp[:2] + self.matching_cp[3:] + [{
+            'cp': (99,),
+            'glyph': 'c',
+            'name': 'LATIN SMALL LETTER C',
+            'idn_table': True,
+            'reference_lgr': True,
+            'result': 'NOTE',
+            'remark': 'Tags not required in Reference LGR'
+        }], key=lambda x: x['cp']))
+
+    def test_repertoire_notes(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_notes.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp[:3] + self.matching_cp[4:] + [{
+            'cp': (100,),
+            'glyph': 'd',
+            'name': 'LATIN SMALL LETTER D',
+            'idn_table': True,
+            'reference_lgr': True,
+            'result': 'NOTE',
+            'remark': 'Tags not required in Reference LGR\nRules not required in Reference LGR'
+        }], key=lambda x: x['cp']))
+
+    def test_repertoire_review_rules(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_review_rules.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp[:3] + self.matching_cp[4:] + [{
+            'cp': (100,),
+            'glyph': 'd',
+            'name': 'LATIN SMALL LETTER D',
+            'idn_table': True,
+            'reference_lgr': True,
+            'result': 'REVIEW',
+            'remark': 'Rules do not match'
+        }], key=lambda x: x['cp']))
+
+    def test_repertoire_review_tag_missing(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_review_tag_missing.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp[:9] + self.matching_cp[10:] + [{
+            'cp': (48,),
+            'glyph': '0',
+            'name': 'DIGIT ZERO',
+            'idn_table': True,
+            'reference_lgr': True,
+            'result': 'REVIEW',
+            'remark': 'Tags do not match'
+        }], key=lambda x: x['cp']))
+
+    def test_repertoire_review_tag(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_review_tag.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp[:8] + self.matching_cp[9:] + [{
+            'cp': (105,),
+            'glyph': 'i',
+            'name': 'LATIN SMALL LETTER I',
+            'idn_table': True,
+            'reference_lgr': True,
+            'result': 'REVIEW',
+            'remark': 'Tags do not match'
+        }], key=lambda x: x['cp']))
+
+    def test_repertoire_reviews_and_note(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_reviews_and_note.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp[:9] + self.matching_cp[10:] + [{
+            'cp': (48,),
+            'glyph': '0',
+            'name': 'DIGIT ZERO',
+            'idn_table': True,
+            'reference_lgr': True,
+            'result': 'REVIEW',
+            'remark': 'Tags do not match\nRules do not match'
+        }], key=lambda x: x['cp']))
+
+    def test_repertoire_subset(self):
+        idn = load_lgr('idn_table_review/repertoire', 'repertoire_subset.xml', unidb=self.unidb)
+
+        result = generate_repertoire_report(idn, self.ref)
+
+        self.assertCountEqual(result, sorted(self.matching_cp[:3] + self.matching_cp[4:] + [{
+            'cp': (100,),
+            'glyph': 'd',
+            'name': 'LATIN SMALL LETTER D',
+            'idn_table': False,
+            'reference_lgr': True,
+            'result': 'SUBSET',
+            'remark': 'Match as a subset of repertoire'
+        }], key=lambda x: x['cp']))
