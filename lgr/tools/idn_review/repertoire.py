b1280067b317d2ad4d05d68dfc032b1e31915d66
Julien BERNARD <julien.bernard@viagenie.ca>
2021-03-18 10:04:30 -0400

    Clean tag handling in IDN repertoire review

--- lgr/tools/idn_review/repertoire.py
+++ lgr/tools/idn_review/repertoire.py
@@ -16,11 +16,10 @@ logger = logging.getLogger(__name__)
 
 class RepertoireReport:
 
-    def __init__(self, idn_table_char: Char, reference_lgr_char: Char, reference_lgr_tags: Set[str],
+    def __init__(self, idn_table_char: Char, reference_lgr_char: Char,
                  reference_lgr_rules_names: Set[str], unidb: UnicodeDatabase):
         self.idn_table_char = idn_table_char
         self.reference_lgr_char = reference_lgr_char
-        self.reference_lgr_tags = reference_lgr_tags
         self.reference_lgr_rules_names = reference_lgr_rules_names
         self.unidb = unidb
 
@@ -41,15 +40,10 @@ class RepertoireReport:
                 result = IdnReviewResult.REVIEW
                 remark = "Tags do not match"
                 break
-            if tag not in self.reference_lgr_char.tags:
-                if tag in self.reference_lgr_tags:
-                    result = IdnReviewResult.REVIEW
-                    remark = "Tags do not match"
-                    break  # stop here as we have a REVIEW that is prioritized over NOTE
-                elif not result:
-                    result = IdnReviewResult.NOTE
-                    remark = "Tags not required in Reference LGR"
-                    # continue in case we have a REVIEW later
+            if tag not in self.reference_lgr_char.tags and not result:
+                result = IdnReviewResult.NOTE
+                remark = "Tags not required in Reference LGR"
+                # continue in case we have a REVIEW later
         # check rule
         idn_rule_name = None
         if self.idn_table_char.when:
@@ -106,9 +100,7 @@ def generate_repertoire_report(idn_table: LGR, reference_lgr: LGR) -> List[Dict]
         idn_char = idn_table_repertoire.get(cp)
         ref_char = reference_lgr_repertoire.get(cp)
         unidb = idn_table.unicode_database if idn_char else reference_lgr.unicode_database
-        report = RepertoireReport(idn_char, ref_char,
-                                  set(t for t in reference_lgr.all_tags() if not t.startswith('sc:')),
-                                  set(reference_lgr.rules_lookup.keys()), unidb)
+        report = RepertoireReport(idn_char, ref_char, set(reference_lgr.rules_lookup.keys()), unidb)
         reports.append(report.to_dict())
 
     return reports
