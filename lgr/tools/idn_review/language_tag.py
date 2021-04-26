f5f02bfba73d8766862902c57782a4fb0e072b17
Julien BERNARD <julien.bernard@viagenie.ca>
2021-04-26 14:34:39 -0400

    Fix IDN table review language tag part ton accept 3 characters language

--- lgr/tools/idn_review/language_tag.py
+++ lgr/tools/idn_review/language_tag.py
@@ -10,6 +10,7 @@ from language_tags import tags
 
 from lgr.core import LGR
 from lgr.tools.idn_review.utils import IdnReviewResult
+from lgr.utils import tag_to_language_script
 
 logger = logging.getLogger(__name__)
 
@@ -20,31 +21,29 @@ class LanguageTagReport:
         self.idn_table_tag = idn_table_tag
         self.reference_lgr_languages = reference_lgr_languages
 
-    @staticmethod
-    def compare_language(tag1, tag2):
-        return tag1.language and tag2.language and tag1.language.format == tag2.language.format
-
     def compare_with_tag(self, tag: str) -> Tuple[IdnReviewResult, str]:
         if self.idn_table_tag is None:
             return IdnReviewResult.MANUAL_CHECK, "Language tag may be included in the comment"
 
-        if self.idn_table_tag.lower().replace('und-', '') == tag.lower().replace('und-', ''):
+        idn_language, idn_script = tag_to_language_script(self.idn_table_tag)
+        ref_language, ref_script = tag_to_language_script(tag)
+
+        if idn_language == ref_language and idn_script == ref_script:
             return IdnReviewResult.MATCH, "Exact match"
 
         idn_tag = tags.tag(self.idn_table_tag)
-        ref_tag = tags.tag(tag)
-        if self.compare_language(idn_tag, ref_tag):
+        if idn_tag.ERR_UNKNOWN in [e.code for e in idn_tag.errors]:
+            return IdnReviewResult.MANUAL_CHECK, "Language tag may be included in the comment"
+
+        if idn_language and idn_language == ref_language:
             if idn_tag.ERR_SUPPRESS_SCRIPT in [e.code for e in idn_tag.errors]:
                 return (IdnReviewResult.MATCH,
                         "Consider minimizing the tag as per the RFC5646 and IANA subtag registry")
             return IdnReviewResult.MATCH, "Language match"
 
-        if (idn_tag.language and idn_tag.language.script and ref_tag.script and
-                idn_tag.language.script.format == ref_tag.script.format):
+        if (idn_tag.language and idn_tag.language.script and ref_script and
+                idn_tag.language.script.format == ref_script):
             return IdnReviewResult.MATCH, "The language tag in IDN Table relevant to the script tag in Reference LGR"
-
-        if idn_tag.ERR_UNKNOWN in [e.code for e in idn_tag.errors]:
-            return IdnReviewResult.MANUAL_CHECK, "Language tag may be included in the comment"
 
         return IdnReviewResult.REVIEW, "The language tag in IDN Table and Reference LGR are mismatched"
 
--- lgr/utils.py
+++ lgr/utils.py
@@ -6,6 +6,9 @@ from __future__ import unicode_literals
 
 import logging
 
+from language_tags import tags
+from pycountry import languages
+
 from lgr import text_type, wide_unichr
 
 logger = logging.getLogger(__name__)
@@ -199,3 +202,31 @@ if __name__ == "__main__":
 
     logger.addHandler(logging.NullHandler())
     doctest.testmod()
+
+
+def tag_to_language_script(tag, use_suppress_script=False):
+    if '-' in tag:
+        # replace 3 char language isocode by 2 char isocode
+        # XXX Assume lang-script format
+        splitted = tag.split('-', 1)
+        lang = splitted[0]
+        try:
+            lang_lookup = languages.lookup(lang)
+            lang_2 = lang_lookup.alpha_2
+        except (LookupError, AttributeError):
+            lang_2 = lang
+
+        splitted[0] = lang_2
+        tag = '-'.join(splitted)
+
+    tag = tags.tag(tag)
+    script = str(tag.script) if tag.script else ''
+    language = tag.language
+    if use_suppress_script and language and not script:
+        suppress_script = language.data.get('record', {}).get('Suppress-Script')
+        script = suppress_script
+    language = str(tag.language) if tag.language else ''
+    if language.lower() == 'und':
+        language = ''
+
+    return language, script
