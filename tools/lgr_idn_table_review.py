8dee5dbb8fdd4fd9f611ec6390f1c20600f2f727
Julien BERNARD <julien.bernard@viagenie.ca>
2021-04-26 15:58:54 -0400

    Improve IDN table review tool

--- tools/lgr_idn_table_review.py
+++ tools/lgr_idn_table_review.py
@@ -7,8 +7,11 @@ Can review IDN tables with reference LGRs.
 """
 from __future__ import unicode_literals
 
+import json
 import logging
+import os
 import sys
+import tempfile
 from datetime import date
 
 from lgr.tools.idn_review.review import review_lgr
@@ -19,15 +22,17 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    tmpdir = tempfile.mkdtemp(prefix='lgr_idn_table_review_report', suffix=str(date.today()))
     parser = LgrToolArgParser(description='LGR IDN Table review')
     parser.add_common_args()
-    parser.add_argument('-o', '--output', help='Output folder where to generate and store the report',
-                        default='/tmp/lgr-idn-table-review-report/' + str(date.today()))
+    parser.add_argument('-o', '--output', help=f'Output folder where to generate and store the report',
+                        default=tmpdir)
     parser.add_argument('-t', '--idn-table', nargs='+', help='The IDN tables to review', required=True)
     parser.add_argument('-x', '--reference-lgr', nargs='+', help='The reference LGR used to review IDN tables',
                         required=True)
 
     args = parser.parse_args()
+    logger.error(f"Write output in {args.output}")
 
     log_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(stream=sys.stdout, level=log_level)
@@ -49,9 +54,11 @@ def main():
             logger.error("Cannot parse LGR file %s, skip it", idn_table)
         idn_tables.append(lgr)
 
+    os.makedirs(args.output, exist_ok=True)
     for idn_table in idn_tables:
         for reference_lgr in reference_lgrs:
-            review_lgr(idn_table, reference_lgr)
+            with open(os.path.join(args.output, f'{idn_table}_{reference_lgr}.json'), 'w') as f:
+                f.write(json.dumps(review_lgr(idn_table, reference_lgr), indent=4, default=str))
 
 
 if __name__ == '__main__':
