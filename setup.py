17a9c8433af8e8e3a003ccca94fc702f869dd95a
Alexandre Taillon-Desrochers <atdesrochers@cofomo.com>
2026-01-15 15:27:42 -0500

    Add changelog for 7.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(TestCommand):
 
 setup(
     name="lgr-core",
-    version='6.1.3',
+    version='7.0.0',
     author='Cofomo, Viagénie and Wil Tan',
     author_email='int-eng@cofomo.com',
     description="API for manipulating Label Generation Rules",
