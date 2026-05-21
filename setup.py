929d30c77da650a2e7c9b14ca9311083a9260be5
Julien BERNARD <jbernard@cofomo.com>
2026-05-21 10:37:36 -0400

    Prepare release 7.0.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(TestCommand):
 
 setup(
     name="lgr-core",
-    version='7.0.0',
+    version='7.0.1',
     author='Cofomo, Viagénie and Wil Tan',
     author_email='int-eng@cofomo.com',
     description="API for manipulating Label Generation Rules",
